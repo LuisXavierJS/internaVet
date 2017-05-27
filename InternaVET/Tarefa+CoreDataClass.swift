5d1db59ffb9484b450b1afed4c4c94aee5381550
JorgeLuisXS <oojlxavier@gmail.com>
2017-05-28 00:46:36 +0100

    estruturando melhor o projeto, e adicionando protocolo de tarefadata no modelo de tarefa

--- InternaVET/Tarefa+CoreDataClass.swift
+++ InternaVET/Tarefa+CoreDataClass.swift
@@ -10,6 +10,23 @@ import Foundation
 import CoreData
 
 
-public class Tarefa: NSManagedObject {
-
+public class Tarefa: NSManagedObject, TarefaDataProtocol {
+    func getNomeDoAnimal()->String?{
+        return nil
+    }
+    func getRacaDoAnimal()->String?{
+        return nil
+    }
+    func getNomeDaTarefa()->String?{
+        return nil
+    }
+    func getHoraDaTarefa()->String?{
+        return nil
+    }
+    func getTipoDaTarefa()->String?{
+        return nil
+    }
+    func getObservacoesDaTarefa()->String?{
+        return nil
+    }
 }
