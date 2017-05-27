664b634b3e63cb6ef0acc4c1c84594ebb3ded239
JorgeLuisXS <oojlxavier@gmail.com>
2017-05-27 15:00:22 +0100

    criado protocolo de tarefas e criando models

--- InternaVET/TarefaDataProtocol.swift
+++ InternaVET/TarefaDataProtocol.swift
@@ -8,6 +8,15 @@
 
 import UIKit
 
-protocol TarefaDataProtocol: class {
-    
+@objc protocol TarefaDataProtocol: class {
+    func getNomeDoAnimal()->String?
+    func getRacaDoAnimal()->String?
+    func getNomeDaTarefa()->String?
+    func getHoraDaTarefa()->String?
+    func getTipoDaTarefa()->String?
+    func getObservacoesDaTarefa()->String?
+    @objc optional func getDadosDoProprietario()->String?
+    @objc optional func getInicioDaTarefa()->String?
+    @objc optional func getFimDaTarefa()->String?
+    @objc optional func getHoraDaProximaDose()->String?
 }
