027468e29c2e930b0aeab06011f1a5e0ee228695
JorgeLuisXS <oojlxavier@gmail.com>
2017-05-28 15:20:18 +0100

    criando TarefaDAO

--- InternaVET/TarefaDAO.swift
+++ InternaVET/TarefaDAO.swift
@@ -9,5 +9,46 @@
 import UIKit
 
 class TarefaDAO: NSObject {
-
+    fileprivate static var cacheDeTarefas: [Animal] = []
+    
+    fileprivate static func addToCache(animal: Animal){
+        if !self.cacheDeTarefas.contains(where: {$0.idAnimal == animal.idAnimal}){
+            self.cacheDeTarefas.append(animal)
+        }
+    }
+    
+    fileprivate static func getAnimalFromCache(ofId: String)->Animal?{
+        return self.cacheDeTarefas.first(where: {$0.idAnimal == ofId})
+    }
+    
+    fileprivate static func getAnimalFromCoreData(ofId: String)->Animal?{
+        let predicate = NSPredicate(format: "idAnimal = %@", ofId)
+        guard let animal = CoreDataManager.fetchRequest(Animal.self, predicate: predicate).first else {
+            return nil
+        }
+        self.addToCache(animal: animal)
+        return animal
+    }
+    
+    static func fetchAnimal(fromIdAnimal id: String)->Animal?{
+        if let animal = getAnimalFromCache(ofId: id) {
+            return animal
+        }
+        return getAnimalFromCoreData(ofId: id)
+    }
+    
+    static func deleteAnimal(animal: Animal){
+        CoreDataManager.deleteObjects(Animal.self, objects: [animal])
+    }
+    
+    static func deleteAnimal(fromIdAnimal id: String){
+        if let animal = fetchAnimal(fromIdAnimal: id){
+            self.deleteAnimal(animal: animal)
+        }
+    }
+    
+    static func createAnimal()->Animal{
+        let animal = CoreDataManager.createEntity(Animal.self)
+        return animal
+    }
 }
