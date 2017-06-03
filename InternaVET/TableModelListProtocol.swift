ec755894ba770d6eb3c5ec58c496088fffdff6a7
JorgeLuisXS <oojlxavier@gmail.com>
2017-06-03 03:50:49 +0100

    finalizando implementacao do cadastro de tarefas

---
+++ InternaVET/TableModelListProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  TableDataProtocol.swift
+//  InternaVET
+//
+//  Created by Luiz Cesar Lopes on 03/06/17.
+//  Copyright © 2017 Jorge Luis. All rights reserved.
+//
+
+import UIKit
+
+protocol TableDataProtocol {
+    func getTitle()->String
+    func getSubTitle()->String    
+}
