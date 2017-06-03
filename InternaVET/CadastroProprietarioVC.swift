7aadf9089f0a92bf7f207ff30de83b86479f6d2e
JorgeLuisXS <oojlxavier@gmail.com>
2017-06-03 16:18:06 +0100

    criando cadastro de proprietario

--- InternaVET/CadastroProprietarioVC.swift
+++ InternaVET/CadastroProprietarioVC.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class CadastroProprietarioVC: CadastroBaseVC {
+    @IBOutlet weak var nomeProprietarioText: UITextField!
+    @IBOutlet weak var telefoneProprietarioText: UITextField!
+    @IBOutlet weak var emailProprietarioText: UITextField!
+    @IBOutlet weak var enderecoProprietarioText: UITextField!
+    
+    weak var proprietario: Proprietario? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        if self.proprietario != nil {
+            self.setupFieldsFromProprietario()
+        }
         // Do any additional setup after loading the view.
     }
 
@@ -20,7 +28,40 @@ class CadastroProprietarioVC: CadastroBaseVC {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    func setupFieldsFromProprietario(){
+        self.enderecoProprietarioText.text = self.proprietario?.endereco
+        self.emailProprietarioText.text = self.proprietario?.email
+        self.nomeProprietarioText.text = self.proprietario?.nome
+        self.telefoneProprietarioText.text = self.proprietario?.telefone
+    }
     
+    func validarCamposObrigatorios()->Bool{
+        let areValid = !(self.nomeProprietarioText.text!.isEmpty)
+        if !areValid {
+            self.presentAlert(title: "Preenchimento Incompleto!", message: "Todos os campos obrigatórios (com asterístico) devem ser preenchidos!")
+        }
+        return areValid    }
+    
+    func setarDadosDoProprietario(){
+        let proprietario = self.proprietario ?? ProprietarioDAO.createProprietario()
+        if self.proprietario == nil {
+            proprietario.idProprietario = NSDate().toString(withFormat: "HHmmhhyyyyMMddsszzz")
+        }
+        proprietario.nome = self.nomeProprietarioText.text
+        proprietario.email = self.emailProprietarioText.text
+        proprietario.telefone = self.telefoneProprietarioText.text
+        proprietario.endereco = self.enderecoProprietarioText.text
+    }
+    
+    override func saveUpdates() -> Bool {
+        if validarCamposObrigatorios(){
+            self.setarDadosDoProprietario()
+            CoreDataManager.saveContext("Salvando proprietario de nome \(self.nomeProprietarioText.text)")
+            return true
+        }
+        return false
+    }
 
     /*
     // MARK: - Navigation
